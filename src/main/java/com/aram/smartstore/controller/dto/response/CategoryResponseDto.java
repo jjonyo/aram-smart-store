f0cdb9eb934067ee87b23d9e4b4ec3e32367e320
jjonyo <jonghyuk96@cau.ac.kr>
2022-04-27 11:03:30 +0900

    Add JacksonObjectMapper Settings

--- src/main/java/com/aram/smartstore/controller/dto/response/CategoryResponseDto.java
+++ src/main/java/com/aram/smartstore/controller/dto/response/CategoryResponseDto.java
@@ -1,13 +1,10 @@
 package com.aram.smartstore.controller.dto.response;
 
 import com.aram.smartstore.domain.CategoryEntity;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @AllArgsConstructor
@@ -22,9 +19,7 @@ public class CategoryResponseDto {
   private final String useYn;
   private final String creatorId;
   private final String modifierId;
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
   private final LocalDateTime createdAt;
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
   private final LocalDateTime modifiedAt;
 
   public static CategoryResponseDto of(CategoryEntity category) {
